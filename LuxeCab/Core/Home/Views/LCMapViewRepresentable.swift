04e448ea81cdaaecd2246d118e999bcdafe18544
jjcode22 <jjmcoding31@gmail.com>
2024-06-09 17:07:26 +0530

    Fixed mapView camera reset bug

--- LuxeCab/Core/Home/Views/LCMapViewRepresentable.swift
+++ LuxeCab/Core/Home/Views/LCMapViewRepresentable.swift
@@ -12,6 +12,7 @@ struct LCMapViewRepresentable: UIViewRepresentable{
     let mapView = MKMapView()
     let locationManager = LocationManager()
     @EnvironmentObject var locationViewModel: LocationSearchViewModel
+    @State private var isUserInteracting = false
     
     func makeUIView(context: Context) -> some UIView {
         mapView.delegate = context.coordinator
@@ -19,6 +20,10 @@ struct LCMapViewRepresentable: UIViewRepresentable{
         mapView.isZoomEnabled = true
         mapView.showsUserLocation =  true
         mapView.userTrackingMode = .follow
+        
+        // Track user interaction to prevent unwanted map resets
+        mapView.addGestureRecognizer(UIPanGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.handleMapPan)))
+        mapView.addGestureRecognizer(UIPinchGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.handleMapPinch)))
         
         return mapView
     }
@@ -48,11 +53,26 @@ extension LCMapViewRepresentable{
         
         //delegate method for when user updates location
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            guard !parent.isUserInteracting else { return }
+            
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
             
             parent.mapView.setRegion(region, animated: true)
 
         }
+        
+        @objc func handleMapPan() {
+            parent.isUserInteracting = true
+        }
+
+        @objc func handleMapPinch() {
+            parent.isUserInteracting = true
+        }
+
+        func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+            parent.isUserInteracting = false
+        }
+
         
         //MARK: - helpers
         
@@ -62,8 +82,10 @@ extension LCMapViewRepresentable{
             
             let anno = MKPointAnnotation()
             anno.coordinate = coordinate
-            self.parent.mapView.addAnnotation(anno)
-            self.parent.mapView.selectAnnotation(anno, animated: true)
+            parent.mapView.addAnnotation(anno)
+            parent.mapView.selectAnnotation(anno, animated: true)
+            
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
         }
     }
     
